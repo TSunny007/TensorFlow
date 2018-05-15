fc940e121d5e82bd44a86fbc171a3538afe255da
Tarun Sunkaraneni <tarun.sunkaraneni@gmail.com>
2018-05-14 22:07:18 -0600

    Designed out Neural nnetwork

--- Images/cnn_mnist.py
+++ Images/cnn_mnist.py
@@ -38,13 +38,66 @@ def cnn_model_fn(features, labels, mode):
     input_layer = tf.reshape(features["x"], [-1,28,28,1]) # Batch size, image_height, image_width, channels (monochrome=1)
     
     # Convolutional Layer #1
-    conv = tf.layers.conv2d (
+    conv1 = tf.layers.conv2d (
             inputs=input_layer,
             filters=32,
             kernel_size=[5,5],
             padding="same",
             activation=tf.nn.relu)
     
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size[2,2], strides=2)
+    
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d (
+            input=pool1,
+            filters=64,
+            kernel_size=[5,5],
+            padding="same",
+            activation=tf.nn.relu)
+    pool2 = tf.layers.maz_poolin2d(inputs=conv2d, pool_size=[2,2], strides=2)
+    
+     # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1,7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+            input=dense, rate=0.4, training_mode == tf.estimator.ModeKeys.TRAIN)
+    
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout,units=10)
+    
+    predictions = {
+        #Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits,axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`
+        "probabilities": tf.mm.softmax(logits, name="softmax_tensor")
+            }
+    
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return estimator.EstimatorSpec(labels=labels, logits=logits)
+    
+    # One hot encode the results [1, 9, ...] -> 
+    #[[0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1], ...]
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    
+    onehot_labels = tf.one_hot(indecies=tf.cast(labels, tf.int32), depth=10)
+    loss = tf.losses.sparse_softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+    
+    # Configuring the training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+                loss=loss,
+                global_step=tf.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode. loss=loss, train_op=train_op)
+    
+    # Configure the Training Op (for TRAIN mode)
+    eval_metric_ops = {
+            "accuracy": tf.metrics.accuracy (
+                    labels=labels, predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+            mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
     
 if __name__ == "__main__":
     tf.app.run()
